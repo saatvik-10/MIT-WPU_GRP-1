128ed43048eb8f7898704bc0cd327fff187e220f
Ishan Magarde <magardeishan@gmail.com>
2025-12-11 18:15:50 +0530

    content view and onboarding

--- Grp1-iOS/Features/Onboarding/Controllers/OnboardingPageViewController.swift
+++ Grp1-iOS/Features/Onboarding/Controllers/OnboardingPageViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class OnboardingPageViewController: UIPageViewController {
-
+    
+    var pagesData: [OnboardingPage] = []
+    var controllers : [OnboardingContentViewController] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadPageData(){
+        pagesData = [
+            OnboardingPage(
+                title: "What's your investment level",
+                message: "Help us to personalize your investment journey"),
+            OnboardingPage(
+                title: "",
+                message: ""),
+            OnboardingPage(
+                title: "",
+                message: "")
+        ]
     }
-    */
 
 }
