85cbc86a6794dea88fe86909f4e7044b63ee91d6
Ishan Magarde <magardeishan@gmail.com>
2025-12-11 18:14:49 +0530

    onboarding screen

---
+++ Grp1-iOS/Features/Onboarding/Controllers/OnboardingPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingPageViewController.swift
+//  Grp1-iOS
+//
+//  Created by SDC-USER on 11/12/25.
+//
+
+import UIKit
+
+class OnboardingPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
