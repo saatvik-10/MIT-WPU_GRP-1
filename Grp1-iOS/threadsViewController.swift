099ea5d9a33eb47eda8bc1c7cc095a2c8b62f422
SDC-USER <sdc-user@VY-7F-IOSTC-093.local>
2025-12-13 13:35:04 +0530

    updated threads page

--- Grp1-iOS/threadsViewController.swift
+++ Grp1-iOS/threadsViewController.swift
@@ -1,10 +1,6 @@
 
 
 
-
-
-
-
 import UIKit
 
 class threadsViewController: UIViewController {
@@ -31,6 +27,13 @@ class threadsViewController: UIViewController {
         let nib = UINib(nibName: "collectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "collectionViewCell")
         
+        let headerNib = UINib(nibName: "ThreadsHeaderCollectionReusableView", bundle: nil)
+        collectionView.register(
+            headerNib,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: "ThreadsHeaderCollectionReusableView"
+        )
+        
         // Simple flow layout
         if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .vertical
@@ -38,6 +41,9 @@ class threadsViewController: UIViewController {
             layout.sectionInset = UIEdgeInsets(top: 12, left: 0, bottom: 20, right: 0)
             
             layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+            
+            layout.headerReferenceSize = CGSize(width: view.frame.width, height: 100)
+           // layout.sectionHeadersPinToVisibleBounds = true
         }
     }
 }
@@ -65,7 +71,27 @@ extension threadsViewController: UICollectionViewDataSource, UICollectionViewDel
         return cell
     }
 
-  
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+
+        let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: "ThreadsHeaderCollectionReusableView",
+            for: indexPath
+        ) as! ThreadsHeaderCollectionReusableView
+
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            referenceSizeForHeaderInSection section: Int) -> CGSize {
+            
+            return CGSize(width: collectionView.frame.width, height: 150)
+        }
+    
+
    
     
 }
