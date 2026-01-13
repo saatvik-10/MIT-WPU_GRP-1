78b3405c06aadafb4c2cb5b0611ea91b79fc73f6
Ishan Magarde <magardeishan@gmail.com>
2026-01-13 17:12:33 +0530

    model and layout changes

---
+++ Grp1-iOS/Features/Onboarding/Views/InterestCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  InterestCollectionViewCell.swift
+//  Grp1-iOS
+//
+//  Created by SDC-USER on 12/01/26.
+//
+
+import UIKit
+
+class InterestCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var interestIconView: UIImageView!
+    @IBOutlet weak var interestLabel: UILabel!
+    @IBOutlet weak var checkmarkView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
