a8d19d7268f9e996ecec6636b453bcdd0bd4eb11
Ishan Magarde <magardeishan@gmail.com>
2026-01-13 17:04:49 +0530

    setting up 3rd onboarding screen

---
+++ Grp1-iOS/Features/Onboarding/Controllers/InterestCollectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InterestCollectionViewController.swift
+//  Grp1-iOS
+//
+//  Created by SDC-USER on 12/01/26.
+//
+
+import UIKit
+
+class InterestCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
