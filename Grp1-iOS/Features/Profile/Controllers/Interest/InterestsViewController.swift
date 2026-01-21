1370cadd018ade2f8a4fd2e23bd7386cdf95d1a9
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-21 15:22:57 +0530

    added preferences segment

--- Grp1-iOS/Features/Profile/Controllers/Interest/InterestsViewController.swift
+++ Grp1-iOS/Features/Profile/Controllers/Interest/InterestsViewController.swift
@@ -6,7 +6,8 @@ class InterestsViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
 
     let domains = InterestsDataSource.domains
-    let companies = InterestsDataSource.companies
+//    let companies = InterestsDataSource.companies
+    let preferences = InterestsDataSource.preferences
 
     var currentItems: [InterestModel] = []
 
@@ -22,8 +23,8 @@ class InterestsViewController: UIViewController {
         if let navController = segue.destination as? UINavigationController,
            let vc = navController.viewControllers.first as? AddInterestViewController {
             
-            vc.interestType = (segmented.selectedSegmentIndex == 0) ? .domain : .company
-            vc.sourceItems = (segmented.selectedSegmentIndex == 0) ? domains : companies
+            vc.interestType = (segmented.selectedSegmentIndex == 0) ? .domain : .preference
+            vc.sourceItems = (segmented.selectedSegmentIndex == 0) ? domains : preferences
         }
     }
 }
@@ -34,8 +35,8 @@ extension InterestsViewController {
     private func setupSegmented() {
         segmented.removeAllSegments()
         segmented.insertSegment(withTitle: "Domains", at: 0, animated: false)
-        segmented.insertSegment(withTitle: "Companies", at: 1, animated: false)
-        segmented.insertSegment(withTitle: "Preferences", at: 2, animated: false)
+//        segmented.insertSegment(withTitle: "Companies", at: 1, animated: false)
+        segmented.insertSegment(withTitle: "Preferences", at: 1, animated: false)
         segmented.selectedSegmentIndex = 0
         segmented.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
     }
@@ -49,14 +50,18 @@ extension InterestsViewController {
             UINib(nibName: "DomainViewCell", bundle: nil),
             forCellWithReuseIdentifier: "DomainViewCell"
         )
+//        collectionView.register(
+//            UINib(nibName: "CompaniesViewCell", bundle: nil),
+//            forCellWithReuseIdentifier: "CompaniesViewCell"
+//        )
         collectionView.register(
-            UINib(nibName: "CompaniesViewCell", bundle: nil),
-            forCellWithReuseIdentifier: "CompaniesViewCell"
+            UINib(nibName: "PreferencesViewCell", bundle: nil),
+            forCellWithReuseIdentifier: "PreferencesViewCell"
         )
     }
 
     @objc private func segmentChanged() {
-        currentItems = (segmented.selectedSegmentIndex == 0) ? domains : companies
+        currentItems = (segmented.selectedSegmentIndex == 0) ? domains : preferences
         collectionView.reloadData()
     }
 }
@@ -82,10 +87,14 @@ extension InterestsViewController: UICollectionViewDataSource, UICollectionViewD
             cell.configure(model)
             return cell
         } else {
+//            let cell = collectionView.dequeueReusableCell(
+//                withReuseIdentifier: "CompaniesViewCell",
+//                for: indexPath
+//            ) as! CompaniesViewCell
             let cell = collectionView.dequeueReusableCell(
-                withReuseIdentifier: "CompaniesViewCell",
+                withReuseIdentifier: "PreferencesViewCell",
                 for: indexPath
-            ) as! CompaniesViewCell
+            ) as! PreferencesViewCell
             cell.configure(model)
             return cell
         }
@@ -103,7 +112,7 @@ extension InterestsViewController {
 
         let itemSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(0.5),
-            heightDimension: .absolute(95)
+            heightDimension: .absolute(160)
         )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
