6e8f90ac87c6c9e03c6d4413e7a743a470391d7a
Ishan Magarde <magardeishan@gmail.com>
2026-01-21 14:34:36 +0530

    restructured the data

--- Grp1-iOS/Features/Onboarding/Controllers/InterestCollectionViewController.swift
+++ Grp1-iOS/Features/Onboarding/Controllers/InterestCollectionViewController.swift
@@ -8,18 +8,11 @@
 import UIKit
 
 class InterestCollectionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stepLabel: UILabel!
     var onBackTapped: (() -> Void)?
-    var preferences : [OnboardingInterestModel] = [
-        OnboardingInterestModel(icon: "indianrupeesign.gauge.chart.lefthalf.righthalf", title: "Indian Economy", subtitle: "Consumption,inflation , growth"),
-        OnboardingInterestModel(icon: "figure.wave", title:"Personal Finance", subtitle: "Exports, Imports and Trade Balance"),
-        OnboardingInterestModel(icon: "newspaper", title:"Government and Policy", subtitle: "Public Spending and Reforms"),
-        OnboardingInterestModel(icon: "chart.line.uptrend.xyaxis", title:"Stock Markets", subtitle: "Shares ,Indices and Market Cycles"),
-        OnboardingInterestModel(icon: "building.2", title:"Real Estate Economics", subtitle: "Housing Interest rates, demand"),
-        OnboardingInterestModel(icon: "globe.central.south.asia.fill", title:"Global Economy", subtitle: "Exports, Imports and Trade Balance"),
-        OnboardingInterestModel(icon: "banknote", title:"Banking and credit", subtitle: "Loans , Interest rates and Moneyflow"),
-        OnboardingInterestModel(icon: "bitcoinsign.circle", title:"Crypto", subtitle: "Bitcoin, Web3 and Digital Assets"),
-        
-    ]
+    
     
     
     @IBOutlet weak var interestCollectionView: UICollectionView!
@@ -33,7 +26,7 @@ class InterestCollectionViewController: UIViewController {
         interestCollectionView.delegate = self
         interestCollectionView.dataSource = self
         interestCollectionView.allowsMultipleSelection = true
-        
+        stepLabel.layer.cornerRadius = 16
         interestCollectionView.register(
             UINib(nibName: "InterestCollectionViewCell", bundle: nil),
             forCellWithReuseIdentifier: "InterestCollectionViewCell"
