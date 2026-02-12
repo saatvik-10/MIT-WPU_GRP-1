a100330c5a829291281e15a09b4e551cc24e4b7b
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-12 15:28:20 +0530

    fixed get me route

--- Grp1-iOS_backend/src/controllers/profile.controller.ts
+++ Grp1-iOS_backend/src/controllers/profile.controller.ts
@@ -1,4 +1,39 @@
 import type { Context } from 'hono';
 import { prisma } from '../../prisma';
 
-export class Profile {}
+export class Profile {
+  async getProfile(ctx: Context) {
+    const userId = ctx.get('userId');
+
+    if (!userId) {
+      return ctx.json('Unauthorized', 401);
+    }
+
+    try {
+      const profile = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          name: true,
+
+          email: true,
+          phone: true,
+          dob: true,
+          gender: true,
+          profileImageUrl: true,
+          level: true,
+        },
+      });
+
+      if (!profile) {
+        return ctx.json('User not found', 404);
+      }
+
+      return ctx.json(profile, 200);
+    } catch (err) {
+      console.log(err);
+      return ctx.json('Error fetching profile', 500);
+    }
+  }
+}
