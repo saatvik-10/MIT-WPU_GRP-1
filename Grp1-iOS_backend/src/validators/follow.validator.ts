ea8974a581f9ede002e94c25140b040c72ed8969
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-17 14:47:51 +0530

    following validator

--- Grp1-iOS_backend/src/validators/follow.validator.ts
+++ Grp1-iOS_backend/src/validators/follow.validator.ts
@@ -1,5 +1,7 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 export const followSchema = z.object({
-    
-})
+	followingId: z.string().trim().min(1, 'followingId is required'),
+});
+
+export type FollowType = z.infer<typeof followSchema>;
