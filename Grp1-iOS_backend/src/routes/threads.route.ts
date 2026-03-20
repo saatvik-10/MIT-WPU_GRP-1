1bb9c28f828624c683d930bf74c4d7b2c6ba3827
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-20 10:17:43 +0530

    added threads routes

--- Grp1-iOS_backend/src/routes/threads.route.ts
+++ Grp1-iOS_backend/src/routes/threads.route.ts
@@ -5,8 +5,21 @@ import { proxyAuth } from '../proxy';
 const threadRoute = new Hono();
 const controller = new Threads();
 
-threadRoute.get('for-you-threads', controller.getForYouThreads);
-threadRoute.post('following-threads', proxyAuth, controller.getFollowingThreads);
-threadRoute.post('follow', proxyAuth, controller.updateFollow);
+threadRoute.get('/for-you-threads', controller.getForYouThreads);
+
+threadRoute.get('/following-threads', proxyAuth, controller.getFollowingThreads);
+
+threadRoute.post('/follow', proxyAuth, controller.updateFollow);
+
+threadRoute.get('/all-followers', proxyAuth, controller.getAllFollowers);
+threadRoute.get('/all-following', proxyAuth, controller.getAllFollowing);
+
+threadRoute.post('/create-thread', proxyAuth, controller.createThread);
+
+threadRoute.post('/draft', proxyAuth, controller.saveDraft);
+threadRoute.get('/drafts', proxyAuth, controller.getDrafts);
+threadRoute.delete('/draft', proxyAuth, controller.deleteDraft)
+
+threadRoute.delete('/thread', proxyAuth, controller.deleteThread)
 
 export default threadRoute;
