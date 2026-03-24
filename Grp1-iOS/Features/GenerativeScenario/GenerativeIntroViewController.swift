7b3ad1f4ae3c9825d2801560a707cc93c6bea0b3
Ishan Magarde <magardeishan@gmail.com>
2026-03-24 14:42:14 +0530

    GenerativeScenario storybord and vc

---
+++ Grp1-iOS/Features/GenerativeScenario/GenerativeIntroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GenerativeIntroViewController.swift
+//  Grp1-iOS
+//
+//  Created by SDC-USER on 18/03/26.
+//
+
+import UIKit
+
+class GenerativeIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
