da5d769b38479f4bc5ca8681a0f8bff2cae98d42
Ishan Magarde <magardeishan@gmail.com>
2026-05-11 16:33:08 +0530

    draft configure function

--- Grp1-iOS/Features/Threads/Views/DraftCollectionViewCell.swift
+++ Grp1-iOS/Features/Threads/Views/DraftCollectionViewCell.swift
@@ -35,4 +35,20 @@ class DraftCollectionViewCell: UICollectionViewCell {
                 draftImgView.backgroundColor = .systemGray5
             }
         }
+        
+    func configure(imageUrl: String?) {
+        draftImgView.image = nil
+        draftImgView.isHidden = false
+        draftImgView.backgroundColor = .systemGray5
+        
+        guard let urlString = imageUrl, let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            guard let data = data, let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self?.draftImgView.image = image
+                self?.draftImgView.backgroundColor = .clear
+            }
+        }.resume()
+    }
 } 
