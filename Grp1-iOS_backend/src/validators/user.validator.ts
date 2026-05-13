5b2f78a3a6d0d0be80c829af52574ca362daba60
saatvik-10 <saatvikmadan529@gmail.com>
2026-05-13 15:20:19 +0530

    added username regex

--- Grp1-iOS_backend/src/validators/user.validator.ts
+++ Grp1-iOS_backend/src/validators/user.validator.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 export const userSignUpSchema = z.object({
-  name: z.string().min(3, 'Name must be atleast 3 characters long'),
+  name: z
+    .string()
+    .min(3, 'Name must be atleast 3 characters long')
+    .regex(/^[A-Za-z]+$/, 'Username must contain only alphabets'),
   email: z.email('Email is required'),
   password: z.string().min(8, 'Password must be atleast 8 characters long'),
   profileImageUrl: z.string().optional(),
